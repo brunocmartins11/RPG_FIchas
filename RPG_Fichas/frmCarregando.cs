5018a7eafdd5de8cb1e3b41678e30c5e6f0ced6c
Poliester2005 <107966984+Poliester2005@users.noreply.github.com>
2024-08-30 22:16:18 -0300

    feat: lock de tipos de dados e tela de carregamento

---
+++ RPG_Fichas/frmCarregando.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RPG_Fichas
+{
+    public partial class frmCarregando : Form
+    {
+        public frmCarregando()
+        {
+            InitializeComponent();
+            // Customize the form for loading indication (e.g., a spinning wheel or "Loading..." message)
+            this.Text = "Carregando...";
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.ShowInTaskbar = false;
+            this.BackColor = Color.White;
+            this.ClientSize = new Size(200, 100);
+            Label loadingLabel = new Label
+            {
+                Text = "Por favor, aguarde...",
+                AutoSize = true,
+                Location = new Point(50, 40)
+            };
+            this.Controls.Add(loadingLabel);
+        }
+    }
+}
--- RPG_Fichas/obj/Debug/net8.0-windows/RPG_Fichas.AssemblyInfo.cs
+++ RPG_Fichas/obj/Debug/net8.0-windows/RPG_Fichas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPG_Fichas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+838e8c1947f1082dca4d98f694d25441e6ce758e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dff12f243731437c5d94ffbe095f7d22aee74f81")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPG_Fichas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPG_Fichas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
